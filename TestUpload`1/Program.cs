dd8fe9da8cabc8d7397d4d61d65eebce2c8d20eb
RobertNieuwkoop <r.nieuwkoop@roc-nijmegen.nl>
2023-03-06 10:42:45 +0100

    Exercise 1-3

--- TestUpload`1/Program.cs
+++ TestUpload`1/Program.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Once upon a time");
-            Console.WriteLine("I was falling in love");
-            Console.WriteLine("Now I'm only falling apart");
+            Console.WriteLine("Once upon a time \nwas falling in love \nNow I'm only falling apart");
         }
     }
 }
