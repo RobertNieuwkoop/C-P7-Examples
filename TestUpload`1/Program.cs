05832755e561252b5f42e722c05721df4b25410b
RobertNieuwkoop <r.nieuwkoop@roc-nijmegen.nl>
2023-03-06 12:07:47 +0100

    Exercise 1-15

--- TestUpload`1/Program.cs
+++ TestUpload`1/Program.cs
@@ -4,10 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give a number!");
+            Console.WriteLine("Give a string:");
             string input = Console.ReadLine();
-            bool nrInput = Convert.ToBoolean(input);
-            Console.WriteLine("You gave: " + nrInput);
+            string stringInput = input;
+
+            Console.WriteLine("Give an integer:");
+            input = Console.ReadLine();
+            int intInput = Convert.ToInt32(input);
+
+            Console.WriteLine("Give an double:");
+            input = Console.ReadLine();
+            double doubleInput = Convert.ToDouble(input);
+
+            Console.WriteLine("Give a boolean:");
+            input = Console.ReadLine();
+            bool boolInput = Convert.ToBoolean(input);
+
+            Console.WriteLine($"Your string: {stringInput} ");
+            Console.WriteLine($"your int: {@intInput} ");
+            Console.WriteLine($"Your double: {doubleInput} ");
+            Console.WriteLine($"Your boolean: {boolInput} ");
         }
     }
 }
