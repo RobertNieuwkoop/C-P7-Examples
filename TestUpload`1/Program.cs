596bf7657971321e8ea615a77dabf2d62f42b692
RobertNieuwkoop <r.nieuwkoop@roc-nijmegen.nl>
2023-03-06 11:57:54 +0100

    Exercise 1-11

--- TestUpload`1/Program.cs
+++ TestUpload`1/Program.cs
@@ -4,18 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I will tell a story, but I need some information.");
-            Console.WriteLine("Give a name for main character:");
-            string? characterName = Console.ReadLine();
-            Console.WriteLine("Give the character a profession:");
-            // Write your code here:
-            string? characterProfession = Console.ReadLine();
-            Console.WriteLine("Here is the story:");
-            string story = string.Format(@"Once upon a time there was a {1} called {0}
-On her way to work, {0} often pondered what being {1} meant to them.
-When you work as a {1} you meet interesting people.
-{0} enjoys their work as {1}, The end.", characterName, characterProfession);
-            Console.WriteLine(story);
+            Console.WriteLine("Days to Summer");
+            int daysToSummer = 200;
+            Console.WriteLine(daysToSummer);
+            Console.WriteLine("Hours to lunch");
+            double hoursToLunch = 3.5;
+            Console.WriteLine(hoursToLunch);
+            Console.WriteLine("Coding is fun");
+            string codingComment = "It sure is!";
+            Console.WriteLine(codingComment);
         }
     }
 }
