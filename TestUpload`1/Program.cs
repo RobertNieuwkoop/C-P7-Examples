708666ddbc0309975b49967bc0227777656d3cbf
RobertNieuwkoop <r.nieuwkoop@roc-nijmegen.nl>
2023-03-07 10:33:08 +0100

    Exercise 1-16

--- TestUpload`1/Program.cs
+++ TestUpload`1/Program.cs
@@ -4,26 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give a string:");
-            string input = Console.ReadLine();
-            string stringInput = input;
-
-            Console.WriteLine("Give an integer:");
-            input = Console.ReadLine();
-            int intInput = Convert.ToInt32(input);
-
-            Console.WriteLine("Give an double:");
-            input = Console.ReadLine();
-            double doubleInput = Convert.ToDouble(input);
-
-            Console.WriteLine("Give a boolean:");
-            input = Console.ReadLine();
-            bool boolInput = Convert.ToBoolean(input);
-
-            Console.WriteLine($"Your string: {stringInput} ");
-            Console.WriteLine($"your int: {@intInput} ");
-            Console.WriteLine($"Your double: {doubleInput} ");
-            Console.WriteLine($"Your boolean: {boolInput} ");
+            string days = Console.ReadLine();
+            int intDays = Convert.ToInt32(days);
+            int hoursInDay = intDays * 24 * 3600;
+            Console.WriteLine(hoursInDay);
         }
     }
 }
